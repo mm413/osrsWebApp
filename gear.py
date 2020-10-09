9cc7b4d2a1c968906eb9585e914aee0c34ddd798
mm413 <markameade@gmail.com>
2020-10-09 16:32:38 -0400

    Created the base logic that pulls down from the db, and adds all types oof gear into lists based off of equipment slot

---
+++ gear.py
@@ -0,0 +1,54 @@
+# Mark Meade
+# Gear.py
+
+import main
+
+from osrsbox import items_api
+
+
+class Gear:
+    def __init__(self):
+        self.headSlotItems = []
+        self.neckSlotItems = []
+        self.shieldSlotItems = []
+        self.mainHandSlotItems = []
+        self.chestSlotItems = []
+        self.legSlotItems = []
+        self.footSlotItems = []
+        self.handSlotItems = []
+        self.ringSlotItems = []
+        self.ammoSlotItems = []
+        self.capeSlotItems = []
+        self.twohandSlotItems = []
+
+    def loadGear(self):
+        items = items_api.load()
+        for item in items:
+            if item.equipable_by_player:
+                if item.equipment.slot == 'weapon':
+                    self.mainHandSlotItems.append(item)
+                elif item.equipment.slot == '2h':
+                    self.twohandSlotItems.append(item)
+                elif item.equipment.slot == 'ammo':
+                    self.ammoSlotItems.append(item)
+                elif item.equipment.slot == 'body':
+                    self.chestSlotItems.append(item)
+                elif item.equipment.slot == 'cape':
+                    self.capeSlotItems.append(item)
+                elif item.equipment.slot == 'feet':
+                    self.footSlotItems.append(item)
+                elif item.equipment.slot == 'hands':
+                    self.handSlotItems.append(item)
+                elif item.equipment.slot == 'head':
+                    self.headSlotItems.append(item)
+                elif item.equipment.slot == 'legs':
+                    self.legSlotItems.append(item)
+                elif item.equipment.slot == 'neck':
+                    self.neckSlotItems.append(item)
+                elif item.equipment.slot == 'ring':
+                    self.ringSlotItems.append(item)
+                elif item.equipment.slot == 'shield':
+                    self.shieldSlotItems.append(item)
+
+
+
---
+++ main.py
@@ -0,0 +1,12 @@
+# Mark Meade
+# Main.py
+
+import gear as otherFile
+
+from osrsbox import items_api
+items = items_api.load()
+
+g = otherFile.Gear()
+
+g.loadGear()
+
