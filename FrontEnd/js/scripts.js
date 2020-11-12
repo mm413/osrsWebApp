1345e53ba4645f92fdcd346fef6ff690cfc3dfbc
mm413 <markameade@gmail.com>
2020-11-12 11:26:02 -0500

    currently, the xmlhttp request is coming in , but I can't figure out how to parse it. I'm going to try to use fetch instead.

--- FrontEnd/js/scripts.js
+++ FrontEnd/js/scripts.js
@@ -1,17 +1,40 @@
-// Create a request variable and assign a new XMLHttpRequest object to it.
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function headButtonClick() {
+  document.getElementById("headDropdown").classList.toggle("show");
+}
+  
+  // Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
+//getgear test
 function getGear(){
-  var request = new XMLHttpRequest()
+  var request = new XMLHttpRequest();
 
   // Open a new connection, using the GET request on the URL endpoint
-  request.open('GET', 'https://api.osrsbox.com/items', true)
+  request.open('GET', 'https://api.osrsbox.com/items');
 
   request.onload = function () {
-    console.log("here")
-    var data = JSON.parse(this.response)
-    data.forEach((item) => {
-        console.log(item)
-    })
+    console.log("sending request...");
+    var data = JSON.parse(this.response);
+    var jsonObjArray = [];
+    jsonObjArray.push(data);
+    console.log(data);
+    for(var i = 0; i < data.length; i++) {
+      var obj = data[i];
+      console.log(obj.name);
+  }
   }
   // Send request
   request.send()
